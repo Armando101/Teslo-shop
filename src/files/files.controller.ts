370df6b790710c96ac5b470be4e2030477ebe989
Armando Rivera <rivera.armando997@gmail.com>
2024-05-02 16:12:49 -0600

    save file into server

---
+++ src/files/files.controller.ts
@@ -0,0 +1,34 @@
+import {
+  BadRequestException,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter,
+      // limits: {fileSize: 1000},
+      storage: diskStorage({
+        destination: './static/uploads',
+      }),
+    }),
+  )
+  uploadProductImage(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('Make sure the file is an image');
+    }
+
+    return { fileName: file.originalname };
+  }
+}

7298c5ce0ee40a4a2276a984a7b529f3811a9c05
Armando Rivera <rivera.armando997@gmail.com>
2024-05-06 14:12:28 -0600

    auth by jwt id

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -31,7 +31,7 @@ export class AuthService {
       });
       await this.userRepository.save(user);
       delete user.password;
-      return { ...user, token: this.getJwtToken({ email: user.email }) };
+      return { ...user, token: this.getJwtToken({ id: user.id }) };
     } catch (error) {
       this.handleError(error);
     }
@@ -41,7 +41,7 @@ export class AuthService {
     const { password, email } = loginUserDto;
     const user = await this.userRepository.findOne({
       where: { email },
-      select: { email: true, password: true },
+      select: { email: true, password: true, id: true },
     });
 
     if (!user) {
@@ -52,7 +52,7 @@ export class AuthService {
       throw new UnauthorizedException('Credentials are not valid (password)');
     }
 
-    return { ...user, token: this.getJwtToken({ email: user.email }) };
+    return { ...user, token: this.getJwtToken({ id: user.id }) };
   }
 
   private getJwtToken(payload: JwtPayload) {
--- src/auth/interfaces/jwt-payload.interface.ts
+++ src/auth/interfaces/jwt-payload.interface.ts
@@ -1,5 +1,5 @@
 export interface JwtPayload {
-  email: string;
+  id: string;
 
   // You can add more not sensitive data
 }
